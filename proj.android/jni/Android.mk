abb990ab5e37f47bf44715459249cfa5b1646134
anplex <anplex@foxmail.com>
2017-06-14 15:10:38 +0800

    iOS v4.0.0 android v3.0.0

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
-$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
-$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,D:/cocos2d-x-3.14.1)
+$(call import-add-path,D:/cocos2d-x-3.14.1/external)
+$(call import-add-path,D:/cocos2d-x-3.14.1/cocos)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -12,9 +12,17 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+				   ../../Classes/C2DXShareSDK/Android/ShareSDKUtils.cpp \
+				   ../../Classes/C2DXShareSDK/C2DXShareSDK.cpp \
+				   ../../Classes/C2DXShareSDK/Android/JSON/CCJSONConverter.cpp \
+				   ../../Classes/C2DXShareSDK/Android/JSON/cJSON/cJSON.c
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+                    $(LOCAL_PATH)/../../Classes/C2DXShareSDK \
+                    $(LOCAL_PATH)/../../Classes/C2DXShareSDK/Android \
+					$(LOCAL_PATH)/../../Classes/C2DXShareSDK/Android/JSON \
+					$(LOCAL_PATH)/../../Classes/C2DXShareSDK/Android/JSON/cJSON
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -28,5 +28,12 @@ package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
+import android.os.Bundle;
+import cn.sharesdk.ShareSDKUtils;
+
 public class AppActivity extends Cocos2dxActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        ShareSDKUtils.prepare();
+    }
 }
